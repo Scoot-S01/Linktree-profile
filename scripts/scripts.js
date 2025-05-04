1485cd55200727fe7709bcac2993cc9c2c026dd3
PyAde <putraadelio26@gmail.com>
2025-05-04 22:28:34 +0800

    update

---
+++ scripts/scripts.js
@@ -0,0 +1,41 @@
+const texts = [
+  "PyAde",
+  "Developer",
+  "Coding for fun"
+];
+
+const typingElement = document.getElementById("typing-text");
+const typingSpeed = 100;
+const erasingSpeed = 50;
+const nextTextDelay = 2000;
+
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+  const currentText = texts[textIndex];
+
+  if (isDeleting) {
+    typingElement.textContent = currentText.substring(0, charIndex - 1);
+    charIndex--;
+  } else {
+    typingElement.textContent = currentText.substring(0, charIndex + 1);
+    charIndex++;
+  }
+
+  if (!isDeleting && charIndex === currentText.length) {
+    isDeleting = true;
+    setTimeout(type, nextTextDelay);
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    textIndex = (textIndex + 1) % texts.length;
+    setTimeout(type, typingSpeed);
+  } else {
+    setTimeout(type, isDeleting ? erasingSpeed : typingSpeed);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  type();
+});
